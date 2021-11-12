9859186db39cb4fe681f6209994fb1983d44c09c
Paracells <enoalife@hotmail.com>
2021-11-12 23:18:22 +0300

    added base card with redux

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function MovieCard() {
-  return <div></div>;
+function MovieCard({ Title, Year, Poster }) {
+  return (
+    <div>
+      <h2>{Title}</h2>
+      <h3>{Year}</h3>
+      <img src={Poster} alt={Title} />
+    </div>
+  );
 }
 
 export default MovieCard;
--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { MovieCard } from '..';
 
 function MovieListing() {
-  return <div>MovieListing</div>;
+  const { Search: movies } = useSelector((state) => state.movies.movies);
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard key={movie.imdbID} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 export default MovieListing;
+
+/*
+Poster: "https://m.media-amazon.com/images/M/MV5BMTM0NTczMTUzOV5BMl5BanBnXkFtZTYwMzIxNTg3._V1_SX300.jpg"
+Title: "Harry Potter and the Order of the Phoenix"
+Type: "movie"
+Year: "2007"
+imdbID: "tt0373889"
+*/
